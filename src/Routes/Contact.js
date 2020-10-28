ffa4fa261c3a51b754c027253dbd34e0c1fa2915
Travis Boss <travisboss@gmail.com>
2020-10-28 12:16:16 -0500

    update for to ajax request

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,81 +1,66 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux'
+import React from "react";
 
-const Contact = () => {
-  const data = useSelector(state => state.resumeData.data);
-
-  const initialState = {
-    name: '',
-      isRequired: true,
-    email: '',
-    subject: '',
-    message: ''
-  };
-
-  const [inputValues, setInputValues] = useState(initialState);
-
-  const email = (data.main || {email: ''});
-
-  const handleChange = e => {
-    const input = e.target.id;
-    const value = e.target.value;
-
-    const newInputVals = {...inputValues, ...{[input]: value}}
-
-    setInputValues(newInputVals)
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
   }
 
-  return (
-    <section id="contact">
-      <div className="row section-head">
-        <div className="two columns header-col">
-          <h1><span>Get In Touch.</span></h1>
-        </div>
-        <div className="ten columns">
-          <p className="lead">{data.contactmessage}</p>
-        </div>
-      </div>
+  render() {
+    const { status } = this.state;
+    return (
+      <section id="contact">
       <div className="row">
-        <div className="eight columns">
-          <form action="https://formspree.io/f/xoqpwqyk" method="post" id="contactForm" name="contactForm">
+      <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/xoqpwqyk"
+        method="POST"
+      >
             <fieldset>
               <div>
                 <label htmlFor="name">Name <span className="required">*</span></label>
-                <input type="text" size="35" id="name" name="contactName" onChange={e => handleChange(e)} required />
+                <input type="text" size="35" id="name" name="contactName" required/>
               </div>
               <div>
-                <label htmlFor="email" name="_replyto">Email <span className="required">*</span></label>
-                <input type="text" size="35" id="email" name="contactEmail" onChange={e => handleChange(e)} required />
+                <label htmlFor="email">Email <span className="required">*</span></label>
+                <input type="text" size="35" id="email" name="_replyto"  required/>
               </div>
               <div>
                 <label htmlFor="subject">Subject</label>
-                <input type="text" size="35" id="subject" name="_subject" onChange={e => handleChange(e)} />
+                <input type="text" size="35" id="subject" name="_subject"/>
               </div>
               <div>
                 <label htmlFor="message">Message <span className="required">*</span></label>
-                <textarea cols="50" rows="15" id="message" name="contactMessage" onChange={e => handleChange(e)} required ></textarea>
-              </div>
-              <div>
-                <button type='submit' className="submit" value="Send">Submit</button>
-                <span id="image-loader">
-                  <img alt="" src="images/loader.gif" />
-                </span>
+                <textarea cols="50" rows="15" id="message" name="contactMessage" required ></textarea>
               </div>
             </fieldset>
-          <input type="text" name="_gotcha" style={{ display: "none" }} />   
-          </form>
-        </div>
-        <aside className="four columns footer-widgets">
-          <div className="widget widget_contact">
-          <h4>Email</h4>
-          <p className="address">
-            <span>{email.email}</span>
-          </p>
-        </div>
-        </aside>
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button className="submit">Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
       </div>
     </section>
-  );
-}
+    );
+  }
 
-export default Contact;
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
